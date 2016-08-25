b61a0f8b429c258b622eaff88eb266156399728f
Dimitar Nestorov <mitkones@gmail.com>
2016-08-25 22:15:29 +0300

    Whoops

--- src/js/adaptor/global.js
+++ src/js/adaptor/global.js
@@ -1,5 +1,6 @@
 /* jslint node: true */
 /* globals define, window, angular */
+/* eslint no-undef: 2 */
 'use strict';
 
 var ps = require('../main');
@@ -15,46 +16,28 @@ if (typeof define === 'function' && define.amd) {
   }
 }
 
-(function (root, factory) {
-  if (typeof module !== 'undefined' && module.exports) {
-    // CommonJS
-    if (typeof angular === 'undefined') {
-      factory(require('angular'));
-    } else {
-      factory(angular);
-    }
-    module.exports = 'ngDialog';
-  } else if (typeof define === 'function' && define.amd) {
-    // AMD
-    define(['angular'], factory);
-  } else {
-    // Global Variables
-    factory(root.angular);
-  }
-}(this, function (angular) {
-  angular.module("perfectScrollbar")
-    .directive('perfectScroll', ['$interval', '$rootScope', function ($interval, $rootScope) {
-      return {
-        scope: {
-          suppressScrollY: "@",
-          suppressScrollX: "@",
-          useBothWheelAxes: "@",
-          psProblematic: "@"
-        },
-        link: function (scope, elem) {
-          window.Ps.initialize(elem[0], {
-            suppressScrollY: angular.isDefined(scope.suppressScrollY) ? scope.suppressScrollY : false,
-            suppressScrollX: angular.isDefined(scope.suppressScrollX) ? scope.suppressScrollX : false,
-            useBothWheelAxes: angular.isDefined(scope.useBothWheelAxes) ? scope.useBothWheelAxes : false
-          });
-          if(scope.psProblematic === "true") {
+angular.module("perfectScrollbar", [])
+  .directive('perfectScroll', ['$interval', '$rootScope', function ($interval, $rootScope) {
+    return {
+      scope: {
+        suppressScrollY: "@",
+        suppressScrollX: "@",
+        useBothWheelAxes: "@",
+        psProblematic: "@"
+      },
+      link: function (scope, elem) {
+        window.Ps.initialize(elem[0], {
+          suppressScrollY: angular.isDefined(scope.suppressScrollY) ? scope.suppressScrollY : false,
+          suppressScrollX: angular.isDefined(scope.suppressScrollX) ? scope.suppressScrollX : false,
+          useBothWheelAxes: angular.isDefined(scope.useBothWheelAxes) ? scope.useBothWheelAxes : false
+        });
+        if(scope.psProblematic === "true") {
+          window.Ps.update(elem[0]);
+          $interval(function () {
             window.Ps.update(elem[0]);
-            $interval(function () {
-              window.Ps.update(elem[0]);
-            }, 5000);
-          }
-          $rootScope.scrollBody = elem;
+          }, 5000);
         }
-      };
-    }]);
-}));
+        $rootScope.scrollBody = elem;
+      }
+    };
+  }]);
